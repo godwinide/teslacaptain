5226ab7fda0fef72d547a4a11b76ca0f837fc45f
iamgodwinide <iamgodwinide@gmail.com>
2025-08-15 17:02:28 +0100

    first commit

---
+++ resend/sendPasswordResetEmail.js
@@ -0,0 +1,20 @@
+const {Resend} = require("resend");
+
+async function sendPasswordResetEmail(receipient, linkId){
+    const resend = new Resend(process.env.RESEND_API_KEY);
+    await resend.emails.send({
+        headers: {
+            "X-My-Header": "Musk Space"
+        },
+        from: "Space Musk Investment <noreply@teslacaptainxmarket.pro>",
+        subject: "Reset Password",
+        to: [receipient],
+        html: `
+            <p>Click the link below to reset your password:</p>
+            <p><a href="https://teslacaptainxmarket.pro/reset-password?code=${linkId}">Reset Password</a></p>
+            <p>Thank you for using Space Musk Investment!</p>
+            `
+      });
+}
+
+module.exports = sendPasswordResetEmail
